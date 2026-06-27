e05cb2e359c39248f8112dfdccefa37301984e01
Sarita-yoobee8 <270823867@yoobeestudent.ac.nz>
2026-06-28 03:03:48 +0530

    Create app.py

---
+++ BMI Calculator/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    bmi = None
+    category = None
+
+    if request.method == "POST":
+        weight = float(request.form["weight"])
+        height = float(request.form["height"])
+
+        bmi = weight / (height ** 2)
+
+        if bmi < 18.5:
+            category = "Underweight"
+        elif bmi < 25:
+            category = "Normal weight"
+        elif bmi < 30:
+            category = "Overweight"
+        else:
+            category = "Obese"
+
+    return render_template("index.html", bmi=bmi, category=category)
+
+if __name__ == "__main__":
+    app.run(debug=False)
